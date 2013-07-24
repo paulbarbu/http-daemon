e4080e2077bba8556395a2ae435b5cd7c6964314
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2013-07-24 20:22:48 +0300

    HTTPServer now uses HTTPConnection to properly serve clients on different threads

--- httpserver.cpp
+++ httpserver.cpp
@@ -1,30 +1,29 @@
 #include <QDebug>
+#include <QThread>
 
 #include "httpserver.h"
-#include "httpthread.h"
+#include "httpconnection.h"
 
 HTTPServer::HTTPServer(const QString &docRoot, QObject *parent) :
     QTcpServer(parent), docRoot(docRoot)
 {
-    //TODO: check the network and the files owned by my program to see what QTcpServer does
+    //TODO: check the network and the files owned by my program to see what
+    //QTcpServer does
 }
 
-void HTTPServer::incomingConnection(int socketDescriptor){
-    qDebug() << "Incoming connection, sd: " << socketDescriptor;
+void HTTPServer::incomingConnection(int socketDescriptor)
+{
+    //TODO: QThreadPool or QtConcurrent
+    QThread *t = new QThread;
 
-    //TODO: thread pool
-    /*https://qt-project.org/doc/qt-4.8/qthread.html#details
-     *
-     *HTTPThread becomes HTTPConnection and is movedToThread() because:
-     *It is important to remember that a QThread object usually lives in the
-     *thread where it was created, not in the thread that it manages.
-     *This oft-overlooked detail means that a QThread's slots will be executed
-     *in the context of its home thread, not in the context of the thread it is
-     *managing. For this reason, implementing new slots in a QThread subclass is
-     *error-prone and discouraged.
-     */
+    HTTPConnection *httpConnection = new HTTPConnection(socketDescriptor,
+                                                        docRoot);
 
-    HTTPThread *t = new HTTPThread(socketDescriptor, docRoot, this);
+    httpConnection->moveToThread(t);
+
+    connect(t, SIGNAL(started()), httpConnection, SLOT(start()));
+    connect(httpConnection, SIGNAL(closed()), t, SLOT(quit()));
+    connect(t, SIGNAL(finished()), httpConnection, SLOT(deleteLater()));
     connect(t, SIGNAL(finished()), t, SLOT(deleteLater()));
 
     t->start();
