c9331e6cb6666a8a8255848e156770006cfcfd69
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2013-08-03 13:41:36 +0300

    HTTPResponse now allows setting the different parts of the response individually

--- httpresponse.cpp
+++ httpresponse.cpp
@@ -1,15 +1,82 @@
 #include "httpresponse.h"
 
-HTTPResponse::HTTPResponse() : response("HTTP/1.0 %1\r\n\r\n%2")
+HTTPResponse::HTTPResponse()
 {
 }
 
-HTTPResponse::HTTPResponse(const QString &statusLine, const QString &body)
+bool HTTPResponse::setStatusCode(int value)
 {
-    response = response.arg(statusLine, body);
+    int firstDigit = value / 100;
+    if(firstDigit < 1 || firstDigit > 5){
+        return false;
+    }
+
+    statusCode = value;
+
+    return true;
 }
 
-HTTPResponse::HTTPResponse(const QByteArray &statusLine, const QByteArray &body)
+void HTTPResponse::setBody(const QString &value)
 {
-    response = response.arg(statusLine, body);
+    body = value;
 }
+
+void HTTPResponse::setBody(const char *value)
+{
+    body = value;
+}
+
+void HTTPResponse::setBody(const QByteArray &value)
+{
+    body = value;
+}
+
+QByteArray HTTPResponse::getResponseData()
+{
+    //TODO: memoize the response until a set*() method is called
+
+    QString response;
+    QString statusLine = "HTTP/1.0 %1 %2\r\n";
+    QString headerPattern = "%1: %2\r\n";
+
+    response = statusLine.arg(QString::number(statusCode), reasonPhrase);
+
+    QHash<QString, QString>::const_iterator i;
+    for(i = fields.constBegin(); i != fields.constEnd(); ++i){
+        response += headerPattern.arg(i.key(), i.value());
+    }
+
+    response += "\r\n" + body;
+
+    return response.toUtf8();
+}
+
+void HTTPResponse::setReasonPhrase(const QString &value)
+{
+    reasonPhrase = value;
+}
+
+void HTTPResponse::setReasonPhrase(const char *value)
+{
+    reasonPhrase = value;
+}
+
+void HTTPResponse::setHeaderField(const QString &key, const QString &value)
+{
+    fields.insert(key, value);
+}
+
+void HTTPResponse::addHeaderFields(const QHash<QString, QString> &value)
+{
+    QHash<QString, QString>::const_iterator i;
+
+    for(i = value.constBegin(); i != value.constEnd(); ++i){
+        setHeaderField(i.key(), i.value());
+    }
+}
+
+void HTTPResponse::setReasonPhrase(const QByteArray &value)
+{
+    reasonPhrase = value;
+}
+
--- httpresponse.h
+++ httpresponse.h
@@ -3,16 +3,28 @@
 
 #include <QByteArray>
 #include <QString>
+#include <QHash>
 
 class HTTPResponse
 {
 public:
     HTTPResponse();
-    HTTPResponse(const QString &statusLine, const QString &body="");
-    HTTPResponse(const QByteArray &statusLine, const QByteArray &body="");
+    bool setStatusCode(int value); //TODO: replace with an enum
+    void setReasonPhrase(const QString &value);
+    void setReasonPhrase(const QByteArray &value);
+    void setReasonPhrase(const char *value);
+    void setBody(const QString &value);
+    void setBody(const QByteArray &value);
+    void setBody(const char *value);
+    void setHeaderField(const QString &key, const QString &value);
+    void addHeaderFields(const QHash<QString, QString> &value);
+    QByteArray getResponseData();
 
 private:
-    QString response;
+    QString body;
+    QString reasonPhrase;
+    int statusCode;
+    QHash<QString, QString> fields;
 };
 
 #endif // HTTPRESPONSE_H
